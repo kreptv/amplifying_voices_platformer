3ca71b595990ed64c67045fcb5e5656d34ce7508
jsb288 <99920511+jsb288@users.noreply.github.com>
2022-11-05 21:54:18 -0400

    spawn bullets

--- Amplifying_Voices_UNITY/Assets/Scripts/ponterController.cs
+++ Amplifying_Voices_UNITY/Assets/Scripts/ponterController.cs
@@ -8,6 +8,10 @@ public class ponterController : MonoBehaviour
     float clickFinish;
     public GameObject player;
     float Speed = 10.0f;
+
+    public GameObject[] objectsSpawning;
+    public GameObject soundParent;
+    public GameObject projectileTransform;
     
     // Start is called before the first frame update
     void Start()
@@ -19,7 +23,7 @@ public class ponterController : MonoBehaviour
     void Update()
     {
         Vector3 pos = Camera.main.WorldToScreenPoint(transform.position);
-        Vector3 dir = Input.mousePosition - pos;
+        Vector3 dir = (Input.mousePosition - pos).normalized;
         float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
 
@@ -29,6 +33,15 @@ public class ponterController : MonoBehaviour
             Vector3 norm = pos.normalized;
             //player.transform.position = norm;
             //player.GetComponent<Rigidbody2D>().AddForce(dir.normalized * Speed);
+            int objectIndex = Random.Range(0, objectsSpawning.Length - 1);
+            Debug.Log(transform.position);
+            GameObject bullet = Instantiate(objectsSpawning[objectIndex], projectileTransform.transform.position, Quaternion.identity);//, soundParent.transform);
+
+            //bullet.transform.forward = dir;            
+            //bullet.transform.rotation = Quaternion.LookRotation(dir);
+
+            bullet.GetComponent<Rigidbody2D>().AddForce(dir * 10);
+
         }
     }
 

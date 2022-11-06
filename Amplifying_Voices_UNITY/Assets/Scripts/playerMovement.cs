3329366855bad94a23704a596dde1d68a24f0b31
SydenZ-inc <syden.zwaigenbaum@georgebrown.ca>
2022-11-05 20:50:12 -0400

    Jump follows the laws of physics
    We have our Jump King!

--- Amplifying_Voices_UNITY/Assets/Scripts/playerMovement.cs
+++ Amplifying_Voices_UNITY/Assets/Scripts/playerMovement.cs
@@ -6,6 +6,7 @@ public class playerMovement : MonoBehaviour
 {
     float speed = 10.0f;
     Rigidbody2D body;
+    bool grounded;
 
     void Awake()
     {
@@ -17,8 +18,16 @@ public class playerMovement : MonoBehaviour
     {
         body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
 
-        if (Input.GetKey(KeyCode.Space)){
+        if (Input.GetKey(KeyCode.Space) && grounded){
             body.velocity = new Vector2(body.velocity.x, speed);
+            grounded = false;
         }
+    }
+
+    // If player is on the ground, you can jump
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+            grounded = true;
     }
 }

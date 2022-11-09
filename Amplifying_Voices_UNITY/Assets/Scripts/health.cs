5c932e51aa260dd786ec73a06b130065a305afc4
jsb288 <99920511+jsb288@users.noreply.github.com>
2022-11-09 02:17:47 -0500

    add hurtbox and health script

---
+++ Amplifying_Voices_UNITY/Assets/Scripts/health.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class health : MonoBehaviour
+{
+    public int currentHealth;
+    public GameObject owner;
+    public string type;
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        owner = transform.parent.gameObject;
+        type = owner.tag;
+    }
+
+    // call this function to add a number to the character's health
+    public int changeHealth(int change){
+        if (type == "Player"){
+            animator.SetBool("IsKnockedBack", true);
+        }
+        currentHealth += change;
+        Debug.Log(owner + " health was changed to " + currentHealth);
+        if (currentHealth <= 0){
+            Debug.Log(owner + " is dead");
+            defeated();
+        }
+        return currentHealth;
+    }
+
+    // Call this when the owner has no health and dies.
+    // Enemies disappear instantly when they die.
+    // Player gets a game over screen when they die.
+    void defeated(){
+        if (type == "Enemy"){
+            Destroy(owner);
+        }else if(type == "Player"){
+            Debug.Log("GAME OVER");
+        }
+    }
+}
